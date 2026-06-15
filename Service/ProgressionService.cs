375d482dd145e2eb8549c07e6f99d7eab87d8e17
youabd123 <yousuf.hh@icloud.com>
2026-06-15 20:08:07 +0200

    Add progression service

--- Service/ProgressionService.cs
+++ Service/ProgressionService.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GymApp.Models;
 
-namespace GymApp.Service
+namespace GymApp.Services;
+
+public class ProgressionService
 {
-    internal class ProgressionService
+    public ExerciseEntry? GetLatestEntryForExercise(GymData data, Guid exerciseId)
     {
+        return data.Sessions
+            .OrderByDescending(session => session.Date)
+            .SelectMany(session => session.Entries)
+            .FirstOrDefault(entry => entry.ExerciseId == exerciseId);
+    }
+
+    public SetEntry? GetBestSetForExercise(GymData data, Guid exerciseId)
+    {
+        return data.Sessions
+            .SelectMany(session => session.Entries)
+            .Where(entry => entry.ExerciseId == exerciseId)
+            .SelectMany(entry => entry.Sets)
+            .OrderByDescending(set => set.Weight)
+            .ThenByDescending(set => set.Reps)
+            .FirstOrDefault();
+    }
+
+    public decimal GetTotalVolumeForExercise(GymData data, Guid exerciseId)
+    {
+        return data.Sessions
+            .SelectMany(session => session.Entries)
+            .Where(entry => entry.ExerciseId == exerciseId)
+            .SelectMany(entry => entry.Sets)
+            .Sum(set => set.Weight * set.Reps);
+    }
+
+    public int GetTotalSetsForExercise(GymData data, Guid exerciseId)
+    {
+        return data.Sessions
+            .SelectMany(session => session.Entries)
+            .Where(entry => entry.ExerciseId == exerciseId)
+            .SelectMany(entry => entry.Sets)
+            .Count();
     }
 }
